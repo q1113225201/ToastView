a3c9c6b59add8e495fc25367574b1ff486aadb5c
林zero <1113225201@qq.com>
2019-01-06 10:45:18 +0800

    ToastUtil

--- app/src/main/java/com/sjl/toastview/MainActivity.java
+++ app/src/main/java/com/sjl/toastview/MainActivity.java
@@ -1,13 +1,55 @@
 package com.sjl.toastview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.sjl.libtoastview.util.ToastUtil;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        findViewById(R.id.btn_toast_msg).setOnClickListener(this);
+        findViewById(R.id.btn_toast_msg_gravity).setOnClickListener(this);
+        findViewById(R.id.btn_toast_view).setOnClickListener(this);
+        findViewById(R.id.btn_toast_view_gravity).setOnClickListener(this);
+    }
+
+    private int cnt = 0;
+
+    @Override
+    public void onClick(View v) {
+        cnt++;
+        switch (v.getId()) {
+            case R.id.btn_toast_msg:
+                ToastUtil.showToast("cnt=" + cnt);
+                break;
+            case R.id.btn_toast_msg_gravity:
+                ToastUtil.showToast("cnt=" + cnt, Gravity.CENTER, 0, 0);
+                break;
+            case R.id.btn_toast_view:
+                ToastUtil.showToast(buildView());
+                break;
+            case R.id.btn_toast_view_gravity:
+                ToastUtil.showToast(buildView(), Gravity.CENTER, 200, 0);
+                break;
+        }
+    }
+
+    private View buildView() {
+        View view = LayoutInflater.from(this).inflate(R.layout.layout_toast_view, null);
+        ((TextView) view.findViewById(R.id.tv_toast)).setText("cnt=" + cnt);
+        return view;
     }
 }
